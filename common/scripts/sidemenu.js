d4e1e67bd8b7e0e82ba94635c1196371816e2096
kanekos99 <pidgeons99@gmail.com>
2025-09-07 23:25:36 +0800

    Separate out side menu

---
+++ common/scripts/sidemenu.js
@@ -0,0 +1,35 @@
+const sideMenuHTML = `
+    <div class="side-menu" id="side-menu">
+      <a class="side-menu-button" href="https://kanekos.neocities.org/"
+        ><i class="fa fa-paw paw-icon" aria-hidden="true"></i>Home</a
+      >
+      <a class="side-menu-button" href="https://kanekos.neocities.org/gallery/"
+        ><i class="fa fa-paw paw-icon" aria-hidden="true"></i>Gallery</a
+      >
+      <a class="side-menu-button" href="https://kanekos.neocities.org/dressup/"
+        ><i class="fa fa-paw paw-icon" aria-hidden="true"></i>Dress Up Game</a
+      >
+    </div>
+`;
+
+$(document).ready(() => {
+  $(sideMenuHTML).insertBefore("#main-body");
+});
+
+var menuOpen = false;
+function toggleSideMenu() {
+  const sideBar = $("#side-menu");
+  const mainBody = $("#main-body");
+
+  if (menuOpen) {
+    sideBar.removeClass("activemenu");
+    mainBody.removeClass("slide-right");
+    $(document.body).removeClass("lock-scroll");
+    menuOpen = false;
+  } else {
+    sideBar.addClass("activemenu");
+    mainBody.addClass("slide-right");
+    $(document.body).addClass("lock-scroll");
+    menuOpen = true;
+  }
+}
--- home/scripts/main.js
+++ home/scripts/main.js
@@ -5,22 +5,3 @@ const app = {
 };
 
 app.init();
-
-const body = document.querySelector("body");
-var sideBar = $("#side-menu");
-var mainBody = $("#main-body");
-
-var menuOpen = false;
-function toggleSideMenu() {
-  if (menuOpen) {
-    sideBar.removeClass("active");
-    mainBody.removeClass("slide-right");
-    $(document.body).removeClass("lock-scroll");
-    menuOpen = false;
-  } else {
-    sideBar.addClass("active");
-    mainBody.addClass("slide-right");
-    $(document.body).addClass("lock-scroll");
-    menuOpen = true;
-  }
-}
