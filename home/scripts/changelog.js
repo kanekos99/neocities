d7b68478ebd2eff6d202fd03bf25c758bc5ec0ef
kanekos99 <pidgeons99@gmail.com>
2025-11-02 23:14:35 +0800

    Dynamically load changelog

---
+++ home/scripts/changelog.js
@@ -0,0 +1,34 @@
+const changelog = [
+  {
+    status: `Added some extra stuff to <a href="https://kanekos.neocities.org/phainon-shrine/">shrine page</a>, added site button and neocities links, dynamically load changelog`,
+    date: "02/11/2025",
+  },
+  {
+    status: `Added a <a href="https://kanekos.neocities.org/phainon-shrine/">shrine page</a>`,
+    date: "29/10/2025",
+  },
+  {
+    status: `Integrated <a href="https://status.cafe/users/kanekos">status.cafe</a> - try hovering over the cat at the top of the page! + added <a href="https://kanekos.neocities.org/guestbook/">guestbook page</a>`,
+    date: "11/09/2025",
+  },
+  {
+    status: `Added placeholder speech bubble for status message`,
+    date: "09/09/2025",
+  },
+  {
+    status: `Styled to do list, added links box, made sidebar button sticky, changed background pattern. Migrated gallery over (although images are still hosted externally on my github pages site)`,
+    date: "07/09/2025",
+  },
+  {
+    status: `The website revamp begins. Added new dress up game page and updated home page. Temporarily removing sketch log page.`,
+    date: "06/09/2025",
+  },
+  {
+    status: `Added WIP home page and sketch log page.`,
+    date: "05/05/2022",
+  },
+  {
+    status: `This website is created.`,
+    date: "29/04/2022",
+  },
+];
--- home/scripts/main.js
+++ home/scripts/main.js
@@ -1,7 +1,29 @@
+const changeLogBox_ID = "#changelog";
+const changelogBox = $(changeLogBox_ID);
+
 const app = {
   init: function () {
     console.log("Hello world :)");
+    loadChangelog();
   },
 };
 
 app.init();
+
+function loadChangelog() {
+  changelog.forEach((log, index) => {
+    let statusLogHTML = `
+      <p class="home-text update-text">
+        <b>${log.date}</b>
+        <br><br>
+        ${log.status}
+      </p>
+    `;
+
+    if (index < changelog.length - 1) {
+      statusLogHTML += `<hr class="dotted-divider-2" />`;
+    }
+
+    changelogBox.append(statusLogHTML);
+  });
+}
