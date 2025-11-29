f552c105bd05ff0c51d7681442dfd629258237d5
kanekos99 <pidgeons99@gmail.com>
2025-11-30 00:47:30 +0800

    Update about page

--- about/scripts/inventory.js
+++ about/scripts/inventory.js
@@ -1,63 +1,163 @@
 const games = [
   {
-    icon: "./assets/phainon-shrine.png",
+    icon: "./assets/inventory/games/hsr.png",
     name: "Honkai Star Rail",
     link: "#",
   },
   {
-    icon: "./assets/phainon-shrine.png",
+    icon: "./assets/inventory/games/genshin.png",
     name: "Genshin Impact",
     link: "#",
   },
   {
-    icon: "./assets/phainon-shrine.png",
+    icon: "./assets/inventory/games/zzz.png",
     name: "Zenless Zone Zero",
     link: "#",
   },
   {
-    icon: "./assets/phainon-shrine.png",
+    icon: "./assets/inventory/games/uma.png",
     name: "Umamusume",
     link: "#",
   },
   {
-    icon: "./assets/phainon-shrine.png",
+    icon: "./assets/inventory/games/gbf.png",
+    name: "Granblue Fantasy",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/games/nu_carnival.png",
+    name: "Nu: Carnival",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/games/acpc.png",
     name: "Animal Crossing: Pocket Camp",
     link: "#",
   },
   {
-    icon: "./assets/phainon-shrine.png",
+    icon: "./assets/inventory/games/acnh.jpg",
     name: "Animal Crossing: New Horizons",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/games/feh.png",
+    name: "Fire Emblem: Heroes",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/games/fe3h.png",
+    name: "Fire Emblem: Three Houses",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/games/ffxiv.png",
+    name: "Final Fantasy XIV",
     link: "#",
   },
 ];
 
 const shows = [
   {
-    icon: "./assets/phainon-shrine.png",
-    name: "Animal Crossing: Pocket Camp",
+    icon: "./assets/inventory/shows/logh.png",
+    name: "Legend of the Galactic Heroes: Die Neue These",
     link: "#",
   },
   {
-    icon: "./assets/phainon-shrine.png",
-    name: "Animal Crossing: New Horizons",
+    icon: "./assets/inventory/shows/trigun.png",
+    name: "Trigun Stampede",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/shows/devilman.png",
+    name: "Devilman Crybaby",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/shows/az.png",
+    name: "Aldnoah.Zero",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/shows/promare.png",
+    name: "Promare",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/shows/maquia.png",
+    name: "Maquia: When the Promised Flower Blooms",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/shows/csm.png",
+    name: "Chainsaw Man – The Movie: Reze Arc",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/shows/mdzs.png",
+    name: "Grandmaster of Demonic Cultivation",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/shows/tgcf.png",
+    name: "Heaven Official's Blessing",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/shows/nezha.png",
+    name: "Nezha",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/shows/tadc.png",
+    name: "The Amazing Digital Circus",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/shows/snowpiercer.png",
+    name: "Snowpiercer",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/shows/xmfc.png",
+    name: "X-Men: First Class",
     link: "#",
   },
 ];
 
 const books = [
   {
-    icon: "./assets/phainon-shrine.png",
-    name: "Zenless Zone Zero",
+    icon: "./assets/inventory/books/dark_rise.jpg",
+    name: "Dark Rise",
     link: "#",
   },
   {
-    icon: "./assets/phainon-shrine.png",
-    name: "Umamusume",
+    icon: "./assets/inventory/books/capri.png",
+    name: "Captive Prince",
     link: "#",
   },
   {
-    icon: "./assets/phainon-shrine.png",
-    name: "Animal Crossing: Pocket Camp",
+    icon: "./assets/inventory/books/erha.png",
+    name: "The Husky and His White Cat Shizun",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/books/yuwu.png",
+    name: "Remnants of Filth",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/books/svsss.png",
+    name: "The Scum Villain's Self-Saving System",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/books/warrior_cats.png",
+    name: "Warrior Cats",
+    link: "#",
+  },
+  {
+    icon: "./assets/inventory/books/blood_bank.png",
+    name: "Blood Bank",
     link: "#",
   },
 ];
--- about/scripts/main.js
+++ about/scripts/main.js
@@ -7,13 +7,26 @@ const shows_container = $(shows_container_name);
 const books_container_name = "#books-container";
 const books_container = $(books_container_name);
 
+const catnekos_gallery_name = "#catnekos_gallery";
+const catnekos_gallery = $(catnekos_gallery_name);
+
+const modalImg = document.getElementById("modal-image");
+
 const inventory_categories = [games, shows, books];
+
+const imageCategories = [
+  {
+    categoryArray: catnekos,
+    categoryElementId: catnekos_gallery,
+  },
+];
 
 const app = {
   init: function () {
     loadInventory(games, games_container);
     loadInventory(shows, shows_container);
     loadInventory(books, books_container);
+    loadImages();
   },
 };
 
@@ -36,4 +49,62 @@ function loadInventory(category, container) {
     `;
     container.append(itemHtml);
   });
+}
+
+/*------------------------ for image gallery -------------------------------- */
+
+function loadImages() {
+  imageCategories.forEach((category) => {
+    category.categoryArray.forEach((image) => {
+      //for neocities
+      let externalUrl = "https://kanekos99.github.io/phainon-shrine" + image;
+
+      let galleryClass = "gallery-thumbnail";
+
+      const imageThumbnailHTML = `
+      <img
+        src="${image}"
+        loading="lazy"
+        class="${galleryClass} img-fluid"
+        onclick="showImage(this)"
+        data-bs-toggle="modal"
+        data-bs-target="#galleryModal"
+      />`;
+
+      category.categoryElementId.append(imageThumbnailHTML);
+    });
+  });
+}
+
+//Lazy Load Option 2 - no small image placeholder
+document.querySelectorAll("img.gallery-thumbnail").forEach((img) => {
+  img.style.opacity = 0; // start hidden
+  img.addEventListener("load", () => {
+    img.style.transition = "opacity 0.7s ease";
+    img.style.opacity = 1; // fade in once loaded
+  });
+});
+
+function showNextOrPrevImg(direction) {
+  const visibleImages = $("img.gallery-thumbnail:visible")
+    .not("#modal-image")
+    .toArray();
+  const currentSrc = modalImg.src;
+  let currentIndex = visibleImages.findIndex((img) => img.src === currentSrc);
+  let nextIndex = currentIndex + direction;
+  if (direction === 1 && nextIndex >= visibleImages.length) {
+    nextIndex = 0;
+  } else if (direction === -1 && nextIndex === -1) {
+    nextIndex = visibleImages.length - 1;
+  }
+  showImage(visibleImages[nextIndex]);
+}
+
+function showImage(image) {
+  modalImg.style.display = "none";
+  modalImg.src = image.src;
+
+  modalImg.onload = function () {
+    modalImg.style.display = "block";
+  };
 }
